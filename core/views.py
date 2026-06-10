fe3321673312cfd41c69684bbd501091584451d2
AtizapanProyectos <soporte.sistemas@atizapan.gob.mx>
2026-06-09 18:37:38 -0600

    Chat de whatsapp y prensa

--- core/views.py
+++ core/views.py
@@ -11,6 +11,8 @@ from django.core.paginator import Paginator
 from django.contrib.auth.decorators import user_passes_test
 import json
 
+from django.shortcuts import render, redirect, get_object_or_404
+
 from django.core.mail import send_mail
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
